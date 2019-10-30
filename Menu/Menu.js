3337f99f3aa5239b8ba5c04d820d886913fa1a22
Thomas Hallberg <thomashallberg@Thomass-Air.attlocal.net>
2019-10-30 11:27:45 -0400

    edit menu function to create the menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -11,25 +11,75 @@ let menuItems = [
 
 /* 
 
-  Step 1: Write a function that will create a menu component as seen below:
+Step 1: Write a function that will create a menu component as seen below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+<div class="menu">
+<ul>
+{each menu item as a list item}
+</ul>
+</div>
 
-  The function takes an array as its only argument.
+The function takes an array as its only argument.
 
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+Add those items to the <ul>
 
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
 
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
-  Step 5: return the menu component.
+Step 5: return the menu component.
 
-  Step 6: add the menu component to the DOM.
-  
+Step 6: add the menu component to the DOM.
+<div class="menu">
+<ul>
+{each menu item as a list item}
+</ul>
+</div>
 */
+
+const header = document.querySelector('.header');
+
+header.appendChild(menuNav(menuItems));
+
+
+function menuNav (array){
+  //define new elements
+  const menuDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  
+  // setup the structure of elements
+  menuDiv.appendChild(ul);
+  
+  //set class name
+  menuDiv.classList.add('menu')
+  
+  //setup for the li
+  array.forEach((arrayItem) => {
+    //Define new elements
+    const li = document.createElement('li');
+    
+    // setup the structure of elements
+    // const ul = document.querySelector('ul')
+    ul.appendChild(li);
+    
+    //Text content
+    li.textContent = arrayItem
+    return li;
+  });
+  
+  //Click event
+  header.addEventListener('click', (e) => {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('menu--open');
+  })
+  
+  return menuDiv;
+};
+
+// const menu = document.querySelector('.menu');
+
+// header.addEventListener('click', (e) => {
+//   menu.classList.toggle('menu--open');
+// })
